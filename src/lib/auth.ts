7d383bdc69f119a4471aa15735b14aee6b838d5f
david <datunebi2003@gmail.com>
2025-10-27 20:29:09 +0200

    Set up prisma Orm + Better auth.

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import prisma from "@prisma/client";
+import {betterAuth} from "better-auth";
+import {prismaAdapter} from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+	database: prismaAdapter(prisma, {
+		provider: 'postgresql',
+	}),
+	emailAndPassword: {
+		enabled: true,
+		autoSignIn: true,
+		minPasswordLength: 8,
+		maxPasswordLength: 32,
+	}
+});
